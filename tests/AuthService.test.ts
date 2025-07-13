6252288555b6b8aa82b5f47f83663b5ce4633094
Otim Isaac <isaacomega16@gmail.com>
2025-07-13 20:05:41 +0300

    reference the token from the mocked data object.

--- tests/AuthService.test.ts
+++ tests/AuthService.test.ts
@@ -5,29 +5,29 @@ import { IPesapalConfig } from '../src/interfaces/IPesapalConfig.interface';
 jest.mock('../src/services/HttpClient');
 
 describe('AuthService', () => {
-  const mockConfig: IPesapalConfig = {
-    consumerKey: 'test_key',
-    consumerSecret: 'test_secret',
-    apiUrl: 'https://test.api',
-    callbackUrl: 'https://test/callback',
-    ipnUrl: 'https://test/ipn',
-    env: 'sandbox'
-  };
+    const mockConfig: IPesapalConfig = {
+        consumerKey: 'test_key',
+        consumerSecret: 'test_secret',
+        apiUrl: 'https://test.api',
+        callbackUrl: 'https://test/callback',
+        ipnUrl: 'https://test/ipn',
+        env: 'sandbox'
+    };
 
-  it('authenticates successfully', async () => {
-    const mockToken = 'test_token';
-    const mockPost = jest.fn().mockResolvedValue({ token: mockToken });
-    (HttpClient as jest.Mock).mockImplementation(() => ({
-      post: mockPost
-    }));
+    it('authenticates successfully', async () => {
+        const mockToken = 'test_token';
+        const mockPost = jest.fn().mockResolvedValue({ data: { token: mockToken } });
+        (HttpClient as jest.Mock).mockImplementation(() => ({
+            post: mockPost
+        }));
 
-    const auth = new AuthService(new HttpClient(mockConfig), mockConfig);
-    const token = await auth.authenticate();
+        const auth = new AuthService(new HttpClient(mockConfig), mockConfig);
+        const token = await auth.authenticate();
 
-    expect(mockPost).toHaveBeenCalledWith('/api/Auth/RequestToken', {
-      consumer_key: mockConfig.consumerKey,
-      consumer_secret: mockConfig.consumerSecret
+        expect(mockPost).toHaveBeenCalledWith('/api/Auth/RequestToken', {
+            consumer_key: mockConfig.consumerKey,
+            consumer_secret: mockConfig.consumerSecret
+        });
+        expect(token).toBe(mockToken);
     });
-    expect(token).toBe(mockToken);
-  });
 });
