d4ef4131dd3bebbf3eeaea0d20586f2a929ff82f
Otim Isaac <isaacomega16@gmail.com>
2025-07-13 18:40:44 +0300

    deleted file with lowercase name

--- src/interfaces/IPesapalConfig.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface IPesapalConfig {
-    consumerKey: string;
-    consumerSecret: string;
-    apiUrl: string;
-    callbackUrl: string;
-    ipnUrl: string;
-    env: 'sandbox' | 'live';
-  }
