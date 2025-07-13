a1ac9a168dd94042b0f9fdad51686cc1a5f6ea9a
Otim Isaac <isaacomega16@gmail.com>
2025-07-13 18:22:00 +0300

    handle client authentication and authorization

---
+++ src/services/AuthService.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from './HttpClient';
+import { IPesapalConfig } from '../interfaces';
+
+export class AuthService {
+  constructor(
+    private http: HttpClient,
+    private config: IPesapalConfig
+  ) {}
+
+  async authenticate() {
+    const response = await this.http.post<{ token: string }>(
+      '/api/Auth/RequestToken',
+      {
+        consumer_key: this.config.consumerKey,
+        consumer_secret: this.config.consumerSecret
+      }
+    );
+    return response.token;
+  }
+}
