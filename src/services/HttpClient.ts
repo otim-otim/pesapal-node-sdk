a38d3c1d41f408ebe212a16f2d86bdc87c0e4675
Otim Isaac <isaacomega16@gmail.com>
2025-07-13 18:23:01 +0300

    setup http client

---
+++ src/services/HttpClient.ts
@@ -0,0 +1,24 @@
+import axios, { AxiosInstance } from 'axios';
+import { IPesapalConfig } from '../interfaces';
+
+export class HttpClient {
+  private client: AxiosInstance;
+  
+  constructor(private config: IPesapalConfig) {
+    this.client = axios.create({
+      baseURL: this.config.apiUrl,
+      headers: { 'Content-Type': 'application/json' }
+    });
+  }
+
+  async post<T>(url: string, data: any, token?: string) {
+    const headers = token ? { Authorization: `Bearer ${token}` } : {};
+    return this.client.post<T>(url, data, { headers });
+  }
+
+  async get<T>(url: string, token: string) {
+    return this.client.get<T>(url, { 
+      headers: { Authorization: `Bearer ${token}` } 
+    });
+  }
+}
