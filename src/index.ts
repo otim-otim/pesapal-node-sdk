45c176b7337ad0abecb36f3a94252f4d833f2acf
Otim Isaac <isaacomega16@gmail.com>
2025-08-21 15:34:59 +0300

    export payment service only

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,20 @@ import getConfig from './config/config';
 import { HttpClient } from './services/HttpClient';
 import { AuthService } from './services/AuthService';
 import { PaymentService } from './services/PaymentService';
+import { IPaymentRequest } from './interfaces/IPayment.interface';
+import { IPesapalConfig } from './interfaces/IPesapalConfig.interface';
 
 const config = getConfig();
 const httpClient = new HttpClient(config);
 const authService = new AuthService(httpClient, config);
 const paymentService = new PaymentService(authService, httpClient, config);
 
-export { paymentService };
+export {
+  paymentService,
+  
+};
+
+export type {
+  IPaymentRequest,
+  IPesapalConfig
+};
