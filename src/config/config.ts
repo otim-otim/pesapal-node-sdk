bce483f59c425f07afbdb0ed5dacc24b10da2751
Otim Isaac <isaacomega16@gmail.com>
2025-09-13 23:59:48 +0300

    fix build errors

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,12 +1,12 @@
 import { IPesapalConfig } from '../interfaces';
 
-const getConfig = (): IPesapalConfig => ({
-  consumerKey: process.env.PESAPAL_CONSUMER_KEY!,
-  consumerSecret: process.env.PESAPAL_CONSUMER_SECRET!,
-  apiUrl: process.env.PESAPAL_ENV === 'sandbox' ? 'https://cybqa.pesapal.com/pesapalv3/api' : 'https://pay.pesapal.com/v3/api',
-  callbackUrl: process.env.PESAPAL_CALLBACK_URL!,
-  ipnUrl: process.env.PESAPAL_IPN_URL!,
-  ipnId: process.env.PESAPAL_IPN_ID!,
+const getConfig = (config?: Partial<IPesapalConfig>): IPesapalConfig => ({
+  consumerKey: config?.consumerKey ?? process.env.PESAPAL_CONSUMER_KEY!,
+  consumerSecret: config?.consumerSecret ?? process.env.PESAPAL_CONSUMER_SECRET!,
+  apiUrl: config?.apiUrl ?? (process.env.PESAPAL_ENV === 'sandbox' ? 'https://cybqa.pesapal.com/pesapalv3/api' : 'https://pay.pesapal.com/v3/api'),
+  callbackUrl: config?.callbackUrl ?? process.env.PESAPAL_CALLBACK_URL!,
+  ipnUrl: config?.ipnUrl ?? process.env.PESAPAL_IPN_URL!,
+  ipnId: config?.ipnId ?? process.env.PESAPAL_IPN_ID!,
 });
 
 export default getConfig;
