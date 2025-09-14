b575db28ecb300828765eb1cded47453cf1f25af
Otim Isaac <isaacomega16@gmail.com>
2025-09-14 15:36:17 +0300

    default to sandbox environment

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ import { IPesapalConfig } from '../interfaces';
 const getConfig = (config?: Partial<IPesapalConfig>): IPesapalConfig => ({
   consumerKey: config?.consumerKey ?? process.env.PESAPAL_CONSUMER_KEY!,
   consumerSecret: config?.consumerSecret ?? process.env.PESAPAL_CONSUMER_SECRET!,
-  apiUrl: config?.apiUrl ?? (process.env.PESAPAL_ENV === 'sandbox' ? 'https://cybqa.pesapal.com/pesapalv3/api' : 'https://pay.pesapal.com/v3/api'),
+  apiUrl: config?.apiUrl ?? (process.env.PESAPAL_ENV === 'live' ? 'https://pay.pesapal.com/v3/api' : 'https://cybqa.pesapal.com/pesapalv3/api'),
   callbackUrl: config?.callbackUrl ?? process.env.PESAPAL_CALLBACK_URL!,
   ipnUrl: config?.ipnUrl ?? process.env.PESAPAL_IPN_URL!,
   ipnId: config?.ipnId ?? process.env.PESAPAL_IPN_ID!,
